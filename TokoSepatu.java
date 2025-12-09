394eb53c3b638170abe0fcb0b31825fb1011655b
GithubFarelAlghazali <farelgithub@gmail.com>
2025-12-10 06:23:52 +0700

    test branch wok bisa engga

--- TokoSepatu.java
+++ TokoSepatu.java
@@ -1,7 +1,7 @@
-import java.util.Scanner;
 
 public class TokoSepatu{
     public static void main(String[] args){
         System.out.println("ALoo gais");
+        System.out.println("aowkwowkoowkwkok");
     }
 }
