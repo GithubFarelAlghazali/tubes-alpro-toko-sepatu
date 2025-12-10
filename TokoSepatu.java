5ef7e7a4f5ad6ab91d4988d735f31daccb703f88
GithubFarelAlghazali <farelgithub@gmail.com>
2025-12-10 07:42:22 +0700

    feat: add user menu

--- TokoSepatu.java
+++ TokoSepatu.java
@@ -1,7 +1,22 @@
 import java.util.Scanner;
 
 public class TokoSepatu{
+
+    static int pilihMenu(Scanner scanner){
+        System.out.println("==== ADMIN TOKO SEPATU LARIS ====");
+        System.out.println("Opsi: ");
+        System.out.println("1. Tampilkan data sepatu");
+        System.out.println("2. Input data sepatu");
+        System.out.println("3. Edit data sepatu");
+        System.out.println("4. Hapus data sepatu");
+        System.out.println("0. Keluar");
+        System.out.print("Pilih opsi: ");
+        int menu = scanner.nextInt();
+        return menu;
+    }
     public static void main(String[] args){
-        System.out.println("ALoo gais");
+        Scanner scanner = new Scanner(System.in);
+        int menu = pilihMenu(scanner );
+        System.out.println(menu);
     }
 }
