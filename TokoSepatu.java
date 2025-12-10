5d46f8b9f4667adadddc52d9912d1f68c54f41c2
GithubFarelAlghazali <farelgithub@gmail.com>
2025-12-10 22:30:07 +0700

    feat: setup edit data

--- TokoSepatu.java
+++ TokoSepatu.java
@@ -1,6 +1,27 @@
 import java.util.Scanner;
 
 public class TokoSepatu{
+    static String[][] dataSepatu = {{ "Nike", "120000", "1km" },{"Rebook", "100000", "90km"},{"Adidas", "250000", "250km"}};
+
+    // Method to display all items in the dataSepatu array
+    static void tampilkanDataSepatu(String[][] dataSepatu) {
+        System.out.println("Daftar Sepatu:");
+        for (int i = 0; i < dataSepatu.length; i++) {
+            for(int j = 0; j < 3; j++){
+                System.out.print(dataSepatu[i][j] + " ");
+            }
+                System.out.println();
+        }
+    }
+
+    static void editData(Scanner scanner){
+        tampilkanDataSepatu(dataSepatu);
+        System.out.println("Pilih data yang ingin diedit");
+        int edited = scanner.nextInt();
+        for(int i = 0; i < 3; i++){
+            System.out.print(dataSepatu[edited - 1][i] + " ");
+        }
+    }
 
     static int pilihMenu(Scanner scanner){
         System.out.println("==== ADMIN TOKO SEPATU LARIS ====");
@@ -18,5 +39,6 @@ public class TokoSepatu{
         Scanner scanner = new Scanner(System.in);
         int menu = pilihMenu(scanner);
         System.out.println(menu);
+        editData(scanner);
     }
 }
