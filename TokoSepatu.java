716e2fa97295e5cc7003ee1847591fc7bba8312e
GithubFarelAlghazali <farelgithub@gmail.com>
2025-12-10 07:29:24 +0700

    Setup scanner

--- TokoSepatu.java
+++ TokoSepatu.java
@@ -2,6 +2,6 @@ import java.util.Scanner;
 
 public class TokoSepatu{
     public static void main(String[] args){
-        System.out.println("ALoo gais");
+        Scanner scanner = new Scanner(System.in);
     }
 }
